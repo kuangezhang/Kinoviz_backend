18bdaff3ff2739e6308773552e31cd3ab75ff6ec
lzhang <zlz200210@gmail.com>
2025-05-12 11:35:21 +0200

    aws

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO movements (id, title, description) VALUES (1, 'Deadlift', 'A strength exercise targeting the posterior chain');
+INSERT INTO movement_body_parts (movement_id, body_part) VALUES (1, 'Back');
